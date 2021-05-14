b272255e10fc9789e7a390678deb6ebafe4fd040
PiaLember <pia.lember@gmail.com>
2021-05-14 15:18:43 -0500

    Add levels of difficulty

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
+import React, { useState } from "react";
 import Game from "./Game";
 
 export default function Header() {
+  let [difficulty, setDifficulty] = useState(null);
+
   return (
     <div className="header">
       <h1>Memory Game</h1>
-      <button>Easy</button>
-      <button>Medium</button>
-      <button>Hard</button>
-      <h3>Choose a difficulty to begin!</h3>
-      <div className="game">
-        <Game />
-      </div>
+      {difficulty === null ? (
+        <div>
+          <button onClick={() => setDifficulty(12)}>Easy</button>
+          <button onClick={() => setDifficulty(18)}>Medium</button>
+          <button onClick={() => setDifficulty(24)}>Hard</button>
+        </div>
+      ) : (
+        <div>
+          <button onClick={() => setDifficulty(null)}>Back</button>
+        </div>
+      )}
+
+      {difficulty ? (
+        <Game difficulty={difficulty} setDifficulty={setDifficulty} />
+      ) : (
+        <h3>Choose a difficulty to begin!</h3>
+      )}
     </div>
   );
 }
