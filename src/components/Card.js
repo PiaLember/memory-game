ddaa42d6264e0a8b606d97b693ae4a85dacfedf8
PiaLember <pia.lember@gmail.com>
2021-05-18 10:04:00 -0500

    Assign color to a flipping card.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,33 @@
-export default function Card() {
-  return <div className="card">Kaart</div>;
+import React, { useState } from "react";
+import { useSpring, animated as a } from "react-spring";
+
+export default function Card(props) {
+  let [flipped, set] = useState(false);
+  let { transform, opacity } = useSpring({
+    opacity: flipped ? 1 : 0,
+    transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
+    config: { mass: 5, tension: 500, friction: 80 },
+  });
+  let game = props.game;
+
+  return (
+    <div className="card" onClick={() => set((state) => !state)}>
+      <a.div
+        className="c back"
+        style={{
+          opacity: opacity.to((o) => 1 - o),
+          transform,
+        }}
+      />
+      <a.div
+        className="c front"
+        style={{
+          opacity,
+          transform,
+          rotateX: "180deg",
+          background: props.color,
+        }}
+      />
+    </div>
+  );
 }
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,11 +1,45 @@
+import React, { useState, useEffect } from "react";
 import Card from "./Card";
 
-export default function Game() {
+export default function Game({ difficulty, setDifficulty }) {
+  let [game, setGame] = useState([]);
+  let color = [
+    { name: "#F82600" },
+    { name: "#FEFE00" },
+    { name: "#2A7F15" },
+    { name: "#1C1FFC" },
+    { name: "#7E7E7E" },
+    { name: "#A734CC" },
+    { name: "#F98003" },
+    { name: "#F8297F" },
+    { name: "#46FDFF" },
+    { name: "#3EFB00" },
+    { name: "#994555" },
+    { name: "#550E84" },
+  ];
+  let color2 = [...color];
+
+  useEffect(() => {
+    let easy = color.copyWithin(6);
+    let medium = color2.slice(0, 9).concat(color2.slice(0, 9));
+    let hard = color2.concat(color2);
+    if (difficulty === 12) {
+      setGame(easy.sort(() => Math.random() - 0.5));
+    } else if (difficulty === 18) {
+      setGame(medium.sort(() => Math.random() - 0.5));
+    } else if (difficulty === 24) {
+      setGame(hard.sort(() => Math.random() - 0.5));
+    }
+  }, []);
+  console.log(game);
+
   return (
-    <div className="row">
-      <div className="col-4">
-        <Card />
-      </div>
+    <div className="cards">
+      {game.map((card, index) => (
+        <div className="card" key={index}>
+          <Card id={index} color={card.name} game={game} />
+        </div>
+      ))}
     </div>
   );
 }
